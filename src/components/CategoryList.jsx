7b1879309539f28993cd5e60d21a03497ad2848e
Aryaman006 <151651851+Aryaman006@users.noreply.github.com>
2024-08-26 13:30:58 +0530

    Update CategoryList.jsx

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -23,7 +23,7 @@ const CategoryList = ({ categories }) => {
                 <Image
                   src={cat.image}
                   alt={`${cat.category} image`}
-                  layout="fill"
+                  layout="intrinsic"
                   sizes="20vw"
                   className="object-cover"
                 />
